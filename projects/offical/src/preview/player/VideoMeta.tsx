fd53793b4b3c94f35a79e6bc92ae7c0c35fc596a
xuhongbin <xuhongbin@yy.com>
2022-08-22 22:00:44 +0800

    docs: up

--- projects/offical/src/preview/player/VideoMeta.tsx
+++ projects/offical/src/preview/player/VideoMeta.tsx
@@ -1,7 +1,7 @@
 import {useEffectStore} from 'src/preview/store/usePlayerStore'
 const VideoMeta = () => {
   const effect = useEffectStore(state => state)
-  console.log('effect.data', effect.data)
+  //   console.log('effect.data', effect.data)
   return (
     <>
       <pre>{JSON.stringify(effect.data, null, 2)}</pre>
