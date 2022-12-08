323c93180dd7e9e153d6b3d4b999989b7974f03e
xuhongbin <xuhongbin@yy.com>
2022-12-08 17:48:42 +0800

    chore: 切回vite 支持windows

--- projects/offical/src/bootstrap.tsx
+++ projects/offical/src/bootstrap.tsx
@@ -1,5 +1,5 @@
 import {createRoot} from 'react-dom/client'
 import PreviewLayout from './preview'
-const rootElm = document.getElementById('emp-root') as HTMLElement
+const rootElm = document.getElementById('root') as HTMLElement
 const root = createRoot(rootElm)
 root.render(<PreviewLayout />)
