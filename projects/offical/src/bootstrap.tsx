d4e85d9995a9a928973821a40fc631cd3c453c98
zhanghaifeng2 <zhanghaifeng2@yy.com>
2024-01-23 18:48:23 +0800

    feat: 支持cef内展示

--- projects/offical/src/bootstrap.tsx
+++ projects/offical/src/bootstrap.tsx
@@ -3,3 +3,8 @@ import PreviewLayout from './preview'
 const rootElm = document.getElementById('root') as HTMLElement
 const root = createRoot(rootElm)
 root.render(<PreviewLayout />)
+
+const win: any = window
+if (win?.CefWindow) {
+  win.CefWindow.show()
+}
