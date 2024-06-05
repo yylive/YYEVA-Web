203150977d687434bf82c1e2c29c8c4bf25fea56
zhanghaifeng2 <zhanghaifeng2@yy.com>
2024-06-05 20:42:15 +0800

    fix: canvas2d渲染明亮度偏暗的问题

--- projects/offical/emp.config.ts
+++ projects/offical/emp.config.ts
@@ -1,9 +1,9 @@
 import {defineConfig} from '@empjs/cli'
 import pluginReact from '@empjs/plugin-react'
-import lightningcssPlugin from '@empjs/plugin-lightningcss'
+// import lightningcssPlugin from '@empjs/plugin-lightningcss'
 export default defineConfig(() => {
   return {
-    plugins: [pluginReact(), lightningcssPlugin()],
+    plugins: [pluginReact()],
     html: {
       template: 'src/index.html',
     },
