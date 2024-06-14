c8f7a9797aa85d7b28a14af006565416d49e08a9
xuhongbin <xuhongbin@yy.com>
2024-06-14 17:58:03 +0800

    feat: up

--- projects/webgpu-demo/src/index.ts
+++ projects/webgpu-demo/src/index.ts
@@ -6,8 +6,9 @@ import './style.css'
 //
 // import './work_01'
 // import './work_02'
-import './work_03'
 //
 // import './learn_01'
 // import './learn_02'
 // import './learn_03'
+//
+import './finish_01'
